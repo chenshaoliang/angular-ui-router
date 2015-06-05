94fdca8c25771e9f41d1e6cbc1c9475a74649a0c
shaoliang <15187518651@139.com>
2015-06-05 11:42:06 +0800

    qinshan

---
+++ scripts/directives.js
@@ -0,0 +1,17 @@
+angular.module('qinshanApp')
+.directive('ngEnter',function() {
+    return function(scope, element, attrs) {
+        element.bind("keydown keypress", function(event) {
+            if(event.which === 13) {
+                scope.$apply(function(){
+                    scope.$eval(attrs.ngEnter, {'event': event});
+                });
+
+                event.preventDefault();
+            }
+        });
+    };
+})
+// .directive('wActive',function(){
+	
+// })
